1fc7181d8f2b16059819432c326ecb72b439cbbc
dexterkun16 <dexterkun16@users.noreply.github.com>
2020-03-01 17:29:07 +0800

    first commit

---
+++ script/script.js
@@ -0,0 +1,13 @@
+const burger = document.getElementsByClassName('burger')[0]
+const navLinks = document.getElementsByClassName('nav-links')[0]
+const nav = document.getElementsByTagName('nav')[0]
+const navLogo = document.getElementsByClassName('nav-logo-alt')[0]
+const body = document.getElementsByTagName('body')[0]
+    //toggle nav
+    burger.addEventListener('click', () => {
+        navLinks.classList.toggle('nav-burger-active');
+        nav.classList.toggle('nav-burger-active_nav-color');
+        navLogo.classList.toggle('nav-logo-p');
+        body.classList.toggle('body');
+    })
+    //animate links
