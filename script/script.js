dd7149d62d904e4c2fd26026dd11e15bb40157fb
dexterkun16 <dexterkun16@users.noreply.github.com>
2020-03-13 15:24:14 +0800

    .

--- script/script.js
+++ script/script.js
@@ -2,12 +2,14 @@ const burger = document.getElementsByClassName('burger')[0]
 const navLinks = document.getElementsByClassName('nav-links')[0]
 const nav = document.getElementsByTagName('nav')[0]
 const navLogo = document.getElementsByClassName('nav-logo-alt')[0]
+const burgerActive = document.getElementsByClassName('burger-active') [0]
 const body = document.getElementsByTagName('body')[0]
     //toggle nav
     burger.addEventListener('click', () => {
+        burger.classList.toggle('burger__active');
         navLinks.classList.toggle('nav-burger-active');
         nav.classList.toggle('nav-burger-active_nav-color');
-        navLogo.classList.toggle('nav-logo-p');
+        navLogo.classList.toggle('nav-logo-alt_active');
         body.classList.toggle('body');
     })
     //events slider
